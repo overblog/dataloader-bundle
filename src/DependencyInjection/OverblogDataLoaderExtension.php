170a316f712d9d2e2c3a2375af984d82b5dfb9f5
Jeroen de Graaf <jeroen@ticketswap.com>
2024-07-01 13:42:46 +0200

    Update `Extension` to meet Symfony 7.1 deprecation
    Symfony 7.1 deprecation:
    
    The "Symfony\Component\HttpKernel\DependencyInjection\Extension" class is
    considered internal since Symfony 7.1, to be deprecated in 8.1;
    use Symfony\Component\DependencyInjection\Extension\Extension instead.

--- src/DependencyInjection/OverblogDataLoaderExtension.php
+++ src/DependencyInjection/OverblogDataLoaderExtension.php
@@ -13,9 +13,9 @@ namespace Overblog\DataLoaderBundle\DependencyInjection;
 
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\DependencyInjection\Reference;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
 final class OverblogDataLoaderExtension extends Extension
 {
